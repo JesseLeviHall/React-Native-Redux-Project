09e43788e9fcb41eb347ca824d4f7b78cdd15833
jesse h <jesselevihall@gmail.com>
2022-03-07 11:05:12 -0700

    Add Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Reservation from "./ReservationComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
+import Favorites from "./FavoritesComponent";
 import {
   View,
   Platform,
@@ -165,6 +166,31 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: (
+        <Icon
+          name="heart"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  }
+);
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView
@@ -227,6 +253,17 @@ const MainNavigator = createDrawerNavigator(
         ),
       },
     },
+
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        drawerLabel: "My Favorites",
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="heart" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+
     Contact: {
       screen: ContactNavigator,
       navigationOptions: {
@@ -242,6 +279,7 @@ const MainNavigator = createDrawerNavigator(
       },
     },
   },
+
   {
     drawerBackgroundColor: "#CEC8FF",
     contentComponent: CustomDrawerContentComponent,
